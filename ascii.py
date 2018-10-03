5535a4140f55bda4bf7c13e377791e5cd42e400c
veritas06 <CoiretFirinne@gmail.com>
2018-10-03 08:17:02 -0400

    Adding another graphic & changing the lookup location

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
-i = random.randrange(9) + 1 
-website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
+i = random.randrange(10) + 1 
+website="https://raw.githubusercontent.com/veritas06/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
